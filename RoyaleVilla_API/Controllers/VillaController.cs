adcf51016eeb79be8cf56e2a6e49534dfbc733dc
Mansour <amrmansour750@yahoo.com>
2026-05-02 17:09:29 +0300

    Create first API Controller

--- RoyaleVilla_API/Controllers/VillaController.cs
+++ RoyaleVilla_API/Controllers/VillaController.cs
@@ -2,9 +2,14 @@
 
 namespace RoyaleVilla_API.Controllers
 {
-
+    [Route("api/Villa")]
     [ApiController]
     public class VillaController : ControllerBase
     {
+        [HttpGet()]
+        public string GetVillas()
+        {
+            return "Here are all Villas !";
+        }
     }
 }
