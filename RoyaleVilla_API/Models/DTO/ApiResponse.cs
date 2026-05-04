232bb6456f548ae2809f4a6ae43758fbd5ee015e
Mansour <amrmansour750@yahoo.com>
2026-05-04 15:59:59 +0300

    Add static factory methods to ApiResponse for standardized responses

--- RoyaleVilla_API/Models/DTO/ApiResponse.cs
+++ RoyaleVilla_API/Models/DTO/ApiResponse.cs
@@ -8,5 +8,53 @@
         public TData? Data { get; set; }
         public object? Errors { get; set; }
         public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
+
+
+        public static ApiResponse<TData> Create(bool success, int statusCode, string message, TData? data = default, object? errors = null)
+        {
+            return new ApiResponse<TData>
+            {
+                Success = success,
+                StatusCode = statusCode,
+                Message = message,
+                Data = data,
+                Errors = errors
+            };
+        }
+
+        //Success
+        public static ApiResponse<TData> Ok(string Message,TData data)
+        {
+            return Create(true, 200, Message, data);
+        }
+
+        public static ApiResponse<TData> CreatedAt(string Message, TData data)
+        {
+            return Create(true, 201, Message, data);
+        }
+        public static ApiResponse<TData> NoContent(string Message = "Operation Completed Successfully")
+        {
+            return Create(true, 204, Message);
+        }
+
+        //Client Errors
+        public static ApiResponse<TData> BadRequest(string Message, object? Errors = null)
+        {
+            return Create(false, 400, Message,errors:Errors);
+        }
+        public static ApiResponse<TData> NotFound(string Message = "Resource Not Found")
+        {
+            return Create(false, 404, Message);
+        }
+        public static ApiResponse<TData> Conflict(string Message)
+        {
+            return Create(false, 409, Message);
+        }
+
+        //Generic Errors
+        public static ApiResponse<TData> Error(int statuscode,string Message,object? Errors = null)
+        {
+            return Create(false, statuscode, Message,errors: Errors);
+        }
     }
 }
