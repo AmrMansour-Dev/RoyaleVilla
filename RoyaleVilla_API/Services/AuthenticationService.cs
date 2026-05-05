65a16f53ab53efacd1a2f3696b69240dc89dfd6d
Mansour <amrmansour750@yahoo.com>
2026-05-05 19:23:44 +0300

    Add IAuthService interface and AuthService stub implementation

---
+++ RoyaleVilla_API/Services/AuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using RoyaleVilla_API.Models.DTO;
+
+namespace RoyaleVilla_API.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public Task<bool> IsEmailExistsAsync(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> LoginAsync(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UserDTO> RegisterAsync(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RoyaleVilla_API/Services/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using RoyaleVilla_API.Models.DTO;
+
+namespace RoyaleVilla_API.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<UserDTO> RegisterAsync(RegisterationRequestDTO registerationRequestDTO);
+        Task<LoginResponseDTO> LoginAsync(LoginRequestDTO loginRequestDTO);
+
+        Task<bool> IsEmailExistsAsync(string email);
+    }
+}
