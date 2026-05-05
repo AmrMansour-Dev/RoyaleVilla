4ed7d85b08817f5a6d1b9563594513b4b8958e27
Mansour <amrmansour750@yahoo.com>
2026-05-05 18:21:34 +0300

    Add AuthController with user registration endpoint

---
+++ RoyaleVilla_API/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoyaleVilla_API.Data;
+using RoyaleVilla_API.Models.DTO;
+
+namespace RoyaleVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public AuthenticationController()
+        {
+
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiResponse<IEnumerable<VillaDTO>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+
+        public async Task<ActionResult<ApiResponse<UserDTO>>> Registeration(RegisterationRequestDTO registerationRequestDTO)
+        {
+            //auth services:
+
+
+            return Ok(ApiResponse<UserDTO>.Ok("User Registered Successfully", null!));
+        }
+
+    }
+}
