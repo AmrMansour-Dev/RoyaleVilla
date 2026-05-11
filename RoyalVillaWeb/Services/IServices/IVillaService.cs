53bdf2d0e1e9ba5f0db02b13174a7cc1f1d3c789
Mansour <amrmansour750@yahoo.com>
2026-05-11 18:51:03 +0300

    Passing token parameter in the IVillaService interface

--- RoyalVillaWeb/Services/IServices/IVillaService.cs
+++ RoyalVillaWeb/Services/IServices/IVillaService.cs
@@ -4,10 +4,10 @@ namespace RoyalVillaWeb.Services.IServices
 {
     public interface IVillaService
     {
-        Task<T> GetAllAsync<T>();
-        Task<T> GetAsync<T>(int ID);
-        Task<T> CreateAsync<T>(VillaCreateDTO villaCreateDTO);
-        Task<T> UpdateAsync<T>(VillaUpdateDTO villaUpdateDTO);
-        Task<T> DeleteAsync<T>(int ID);
+        Task<T> GetAllAsync<T>(string Token);
+        Task<T> GetAsync<T>(int ID,string Token);
+        Task<T> CreateAsync<T>(VillaCreateDTO villaCreateDTO, string Token);
+        Task<T> UpdateAsync<T>(VillaUpdateDTO villaUpdateDTO, string Token);
+        Task<T> DeleteAsync<T>(int ID, string Token);
     }
 }
--- RoyalVillaWeb/Services/VillaService.cs
+++ RoyalVillaWeb/Services/VillaService.cs
@@ -5,27 +5,27 @@ namespace RoyalVillaWeb.Services
 {
     public class VillaService : IVillaService
     {
-        public Task<T> CreateAsync<T>(VillaCreateDTO villaCreateDTO)
+        public Task<T> CreateAsync<T>(VillaCreateDTO villaCreateDTO, string Token)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> DeleteAsync<T>(int ID)
+        public Task<T> DeleteAsync<T>(int ID, string Token)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> GetAllAsync<T>()
+        public Task<T> GetAllAsync<T>(string Token)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> GetAsync<T>(int ID)
+        public Task<T> GetAsync<T>(int ID, string Token)
         {
             throw new NotImplementedException();
         }
 
-        public Task<T> UpdateAsync<T>(VillaUpdateDTO villaUpdateDTO)
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO villaUpdateDTO, string Token)
         {
             throw new NotImplementedException();
         }
