0b074d2d5b3a955ffe1ca64f926c6bb815279cda
Mansour <amrmansour750@yahoo.com>
2026-05-11 16:28:53 +0300

    Implement base services

--- RoyalVillaWeb/Services/BaseService.cs
+++ RoyalVillaWeb/Services/BaseService.cs
@@ -20,9 +20,47 @@ namespace RoyalVillaWeb.Services
             ResponseModel = new ApiResponse<object>();
         }
 
-        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        public async Task<T?> SendAsync<T>(ApiRequest apiRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var Client = _httpClient.CreateClient("RoualVillaAPI");
+
+                var message = new HttpRequestMessage()
+                {
+                    Method = GetMethodType(apiRequest.ApiType),
+                    RequestUri = new Uri(apiRequest.Url)
+                };
+                if(apiRequest.Data != null )
+                {
+                    message.Content = JsonContent.Create(apiRequest.Data, options: jsonSerializerOptions); // Serialize object to JSON HTTP content
+                }
+
+                var apiResponse = await Client.SendAsync(message);
+
+                return await apiResponse.Content.ReadFromJsonAsync<T>(jsonSerializerOptions); //Deserialize JSON response body to C# object of type T
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return default;
+            }
+        }
+
+        public static HttpMethod GetMethodType(SD.ApiType apiType)
+        {
+            switch(apiType)
+            {
+                case SD.ApiType.Delete:
+                    return HttpMethod.Delete;
+                case SD.ApiType.Post:
+                    return HttpMethod.Post;
+                case SD.ApiType.Put:
+                    return HttpMethod.Put;
+                default:
+                    return HttpMethod.Get;
+
+            }
         }
     }
 }
