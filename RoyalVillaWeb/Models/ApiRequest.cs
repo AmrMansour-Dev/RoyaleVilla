bd0dab97e150d21b95c5cb5137ed4634e143ceb1
Mansour <amrmansour750@yahoo.com>
2026-05-11 15:28:41 +0300

    Adding Static Details (SD) model in the web project contains enum of HTTP Verbs and adding APIRequest model in the web project with some of properties

---
+++ RoyalVillaWeb/Models/ApiRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace RoyalVillaWeb.Models
+{
+    public class ApiRequest
+    {
+        public SD.ApiType ApiType { get; set; } = SD.ApiType.Get;
+        public string? Url { get; set; }
+        public object? Data { get; set; }
+        public string? Token { get; set; }
+
+    }
+}
---
+++ RoyalVillaWeb/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace RoyalVillaWeb
+{
+    public static class SD
+    {
+        public enum ApiType
+        {
+            Get,
+            Post,
+            Put,
+            Delete
+        }
+    }
+}
