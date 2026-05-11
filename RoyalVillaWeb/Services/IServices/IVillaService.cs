daaff5ec88c2aebcfd4f5a8cf71281cb31febed8
Mansour <amrmansour750@yahoo.com>
2026-05-11 18:26:32 +0300

    Adding IVillaService interface and implement it inside VillaService class

---
+++ RoyalVillaWeb/Services/IServices/IVillaService.cs
@@ -0,0 +1,13 @@
+﻿using RoyalVilla.DTO;
+
+namespace RoyalVillaWeb.Services.IServices
+{
+    public interface IVillaService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetAsync<T>(int ID);
+        Task<T> CreateAsync<T>(VillaCreateDTO villaCreateDTO);
+        Task<T> UpdateAsync<T>(VillaUpdateDTO villaUpdateDTO);
+        Task<T> DeleteAsync<T>(int ID);
+    }
+}
---
+++ RoyalVillaWeb/Services/VillaService.cs
@@ -0,0 +1,33 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Services
+{
+    public class VillaService : IVillaService
+    {
+        public Task<T> CreateAsync<T>(VillaCreateDTO villaCreateDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync<T>(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync<T>(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync<T>(VillaUpdateDTO villaUpdateDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
