2ad5e15789ae72ff74a4ced5b48fd910c355fda9
Mansour <amrmansour750@yahoo.com>
2026-05-13 18:21:37 +0300

    Add VillaController in Web Project - Add Villa Management to Navbar

---
+++ RoyalVillaWeb/Controllers/VillaController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
+using System.Diagnostics;
+
+namespace RoyalVillaWeb.Controllers
+{
+    public class VillaController : Controller
+    {
+        private IVillaService _villaservice;
+
+        public VillaController(IVillaService villaService)
+        {
+            _villaservice = villaService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> villaslist = new List<VillaDTO>();
+            try
+            {
+                var response = await _villaservice.GetAllAsync<ApiResponse<List<VillaDTO>>>("");
+                if(response != null && response.Success && response.Data != null)
+                {
+                    villaslist = response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Errors"] = $"An error occured:{ex.Message}";
+            }
+
+            return View(villaslist);
+        }
+    }
+}
