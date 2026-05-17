0db4a949843a8d32488eda3a03d358c2bb54ab48
Mansour <amrmansour750@yahoo.com>
2026-05-17 09:36:38 +0300

    Add Delete as view in Views and as a an action in VillaController

--- RoyalVillaWeb/Controllers/VillaController.cs
+++ RoyalVillaWeb/Controllers/VillaController.cs
@@ -62,5 +62,51 @@ namespace RoyalVillaWeb.Controllers
 
             return View(villaCreateDTO);
         }
+
+        public async Task<IActionResult> Delete(int ID)
+        {
+            if (ID <= 0)
+            {
+                TempData["Errors"] = "Invalid Villa ID";
+                return RedirectToAction("Index");
+            }
+            try
+            {
+                var response = await _villaservice.GetAsync<ApiResponse<VillaDTO>>(ID, "");
+
+                if (response != null && response.Success && response.Data != null)
+                {
+                    return View(response.Data);
+                }
+            }
+            catch(Exception ex)
+            {
+
+                TempData["Errors"] = $"An error occured:{ex.Message}";
+            }
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(VillaDTO villaDTO)
+        {
+            try
+            {
+                var response = await _villaservice.DeleteAsync<ApiResponse<object>>(villaDTO.Id, "");
+                if (response != null && response.Success)
+                {
+                    TempData["Success"] = $"Villa Deleted Successfully";
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Errors"] = $"An error occured:{ex.Message}";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
