62a020eeb05dd1290305034586b5dd61e4a6e2fe
Mansour <amrmansour750@yahoo.com>
2026-05-17 13:49:17 +0300

    Add Auth Controller with some ActionMethods

---
+++ RoyalVillaWeb/Controllers/AuthController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
+using System.Diagnostics;
+
+namespace RoyalVillaWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        private IAuthService _authaservice;
+
+        public AuthController(IAuthService authService)
+        {
+            _authaservice = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
+        {
+
+            try
+            {
+                var response = await _authaservice.LoginAsync<ApiResponse<LoginResponseDTO>>(loginRequestDTO);
+                if (response != null && response.Success && response.Data != null)
+                {
+                    LoginResponseDTO model = response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Errors"] = $"An error occured:{ex.Message}";
+            }
+
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View(new RegisterationRequestDTO()
+            {
+                Email = string.Empty,
+                Name = string.Empty,
+                Password = string.Empty,
+                Role = "Customer"
+            });
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+    }
+
+}
